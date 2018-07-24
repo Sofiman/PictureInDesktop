2abcbf5106a37a35cb2ad5a443e45068d7dc0130
Sofiane M <sofiane.meftah@outlook.fr>
2018-07-24 20:56:28 +0200

    Removed Debug Code

--- config.js
+++ config.js
@@ -120,7 +120,6 @@ let config = {
             getStreamURL: function getStreamURL(inputURL) {
                 const regex = /^((?:https?:)?\/\/)?((?:www|m|gaming)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/g;
                 let result = regex.exec(inputURL);
-                console.log(result);
                 return result && result[5] ? `https://youtube.com/live_chat?is_popout=1&v=${result[5]}` : undefined;
             },
             force: true
