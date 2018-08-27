bf002e2443f845dd6918305bf10bb823b24419cc
Sofiane M <sofiane.meftah@outlook.fr>
2018-08-27 13:01:41 +0200

    Added a Embed loading indicator

--- render/embed.js
+++ render/embed.js
@@ -5,10 +5,20 @@ let rp = {
 let cw = require('electron').remote.getCurrentWindow();
 let esurl = cw.embedStreamURL;
 let psv = cw.providerService;
+let embedIframe = document.querySelector("iframe#embed");
 
-document.title += ': ' + psv;
-document.querySelector('#embed').src = esurl;
 document.querySelector('.controls').style.top = cw.offsetY + "px";
 if(cw.darkMode) document.querySelectorAll('.controls *').forEach(el => el.classList.add('dark'));
 document.querySelector('#open').href = rp[psv] ? esurl.replace(rp[psv][0], rp[psv][1]) : esurl;
 document.querySelector('#close').addEventListener('click', () => cw.closeAll());
+
+embedIframe.addEventListener('load', () => {
+    document.getElementById('loader').style.display = 'none';
+    var doc = embedIframe.contentDocument;
+    var style = doc.createElement("style");
+    style.setAttribute('data-author', 'PictureInDesktop');
+    style.type = 'text/css';
+    style.appendChild(doc.createTextNode('body{margin:0}.gist-file{margin-bottom:0!important}'));
+    doc.head.appendChild(style);
+});
+embedIframe.src = esurl;
