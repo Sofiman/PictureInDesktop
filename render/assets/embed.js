512f357db33d4ea068a3114ed2a7a5359bda6021
Sofiane M <sofiane.meftah@outlook.fr>
2019-01-27 19:01:12 +0100

    Added a small effect to show hovering and Refactored code

--- render/assets/embed.js
+++ render/assets/embed.js
@@ -1,28 +1,29 @@
-const electron = require('electron');
-const {ipcRenderer: ipc} = electron;
+(function () {
+    const electron = require('electron');
 
-let rp = {
-    'Youtube': ['/embed', 'watch?v='],
-    'Twitch': ['http://player.twitch.tv/?channel=', 'https://twitch.tv/'],
-    'Gist': [/data:text\/html,<script src="(.*)\.js"><\/script>/, '$c1'],
-    'File': ['', '']
-};
-let cw = electron.remote.getCurrentWindow();
-let esurl = cw.embedStreamURL;
-let psv = cw.providerService;
-let embedIframe = document.querySelector("#embed");
+    const rp = {
+        'Youtube': ['/embed/', '/watch?v='],
+        'Twitch': ['http://player.twitch.tv/?channel=', 'https://twitch.tv/'],
+        'Gist': [/data:text\/html,<script src="(.*)\.js"><\/script>/, '$c1'],
+        'File': ['', '']
+    };
+    const cw = electron.remote.getCurrentWindow();
+    const esurl = cw.embedStreamURL, psv = cw.providerService;
+    let embedIframe = document.querySelector("#embed");
 
-document.querySelector('.controls').style.top = cw.offsetY + "px";
-if(cw.darkMode) document.querySelectorAll('.controls *').forEach(el => el.classList.add('dark'));
-document.querySelector('#open').href = rp[psv] ? esurl.replace(rp[psv][0], rp[psv][1]) : esurl;
-document.querySelector('#close').addEventListener('click', () => cw.closeAll());
+    document.querySelector('.controls').style.top = cw.offsetY + "px";
+    if (cw.darkMode) document.querySelectorAll('.controls *').forEach(el => el.classList.add('dark'));
+    document.querySelector('#open').href = rp[psv] ? esurl.replace(rp[psv][0], rp[psv][1]) : esurl;
+    document.querySelector('#close').addEventListener('click', () => cw.closeAll());
 
-embedIframe.addEventListener('did-stop-loading', () => {
-    document.getElementById('loader').style.display = 'none';/*
-    let doc = embedIframe.contentDocument, style = doc.createElement("style");
-    style.setAttribute('data-author', 'PictureInDesktop');
-    style.type = 'text/css';
-    style.appendChild(doc.createTextNode('body{margin:0}.gist-file{margin-bottom:0!important}'));
-    doc.head.appendChild(style);*/
-});
-embedIframe.src = esurl;
+    embedIframe.addEventListener('did-stop-loading', () => {
+        document.getElementById('loader').style.display = 'none';
+        /*
+            let doc = embedIframe.contentDocument, style = doc.createElement("style");
+            style.setAttribute('data-author', 'PictureInDesktop');
+            style.type = 'text/css';
+            style.appendChild(doc.createTextNode('body{margin:0}.gist-file{margin-bottom:0!important}'));
+            doc.head.appendChild(style);*/
+    });
+    embedIframe.src = esurl;
+})();
